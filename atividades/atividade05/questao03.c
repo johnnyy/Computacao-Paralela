ad0979ca56c2fbf32af08b1b2a707f6f747ff178
Johnny Marcos <johnnymarcos1998@gmail.com>
2020-08-19 19:03:06 -0300

    atividade 5

--- atividades/atividade05/questao03.c
+++ atividades/atividade05/questao03.c
@@ -8,12 +8,11 @@
 
 int main(int argc, char *argv[]) {
     double parcial = 0.0, total= 0.0;
-    int source, destination = 0;
-    int tag = 3;
-    double starttime, endtime;
+    int source = 0, destination = 0;
+    int tag = 0;
     int rank, size, p, lo, hi;
 
-    starttime = MPI_Wtime();
+
 
 
    
@@ -26,22 +25,33 @@ int main(int argc, char *argv[]) {
     long int n = atoi(argv[1]);
     p = n/size;
 
-    double * vector1 = malloc(p*sizeof(double));
-    double * vector2 = malloc(p*sizeof(double));
-
-    for(int j = 0; j < p ; j++){
-        vector1[j] = 1.0;
-        vector2[j] = 1.0;
-
-
-    }
-    for(int i = 0; i<p; i++){
-        parcial += vector1[i]*vector2[i];
-    }
-
    
-    if(rank ==0){      
+    if(rank ==0){
+	double * vector1 = malloc(n*sizeof(double));
+	double * vector2 = malloc(n*sizeof(double));
+
+	for(int j = 0; j < n ; j++){
+		vector1[j] = 1.0;
+		vector2[j] = 1.0;
+
+	}
+
+
+	for (int destination = 1; destination < size; destination++) {
+
+
+        	MPI_Send(vector1, p, MPI_DOUBLE, destination, tag, MPI_COMM_WORLD);
+        	MPI_Send(vector2 , p, MPI_DOUBLE, destination, tag+1, MPI_COMM_WORLD);
+        }
+
+
+	for(int i = 0; i<p; i++){
+		parcial += vector1[i]*vector2[i];
+	}
+
+
         total = parcial;
+
         for (source = 1; source < size; source++) {
             MPI_Recv(&parcial, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
             total += parcial;
@@ -49,9 +59,20 @@ int main(int argc, char *argv[]) {
         printf("Total: %lf\n",total);
     }
 
+
+
     else{
-        
-        MPI_Send(&parcial, 1, MPI_DOUBLE, destination, tag, MPI_COMM_WORLD);
+	double * vector1 = malloc(p*sizeof(double));
+	double * vector2 = malloc(p*sizeof(double));
+
+        MPI_Recv(vector1, p, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+        MPI_Recv(vector2, p, MPI_DOUBLE, 0, tag+1, MPI_COMM_WORLD, &status);
+
+	for(int i = 0; i<p; i++){
+       		parcial += vector1[i]*vector2[i];
+    	}
+
+        MPI_Send(&parcial, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD);
 
 
     }
--- atividades/atividade05/questao04.c
+++ atividades/atividade05/questao04.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <math.h>
 #include <mpi.h>
@@ -5,11 +7,10 @@
 #include <stdlib.h>
 
 
-
 int main(int argc, char *argv[]) {
     double parcial = 0.0, total= 0.0;
-    int source, destination = 0;
-    int tag = 3;
+    int source = 0, destination = 0;
+    int tag = 0;
     double starttime, endtime;
     int rank, size, p, lo, hi;
 
@@ -26,34 +27,56 @@ int main(int argc, char *argv[]) {
     long int n = atoi(argv[1]);
     p = n/size;
 
-    double * vector1 = malloc(p*sizeof(double));
-    double * vector2 = malloc(p*sizeof(double));
-
-    for(int j = 0; j < p ; j++){
-        vector1[j] = 1.0;
-        vector2[j] = 1.0;
-
-
-    }
-    for(int i = 0; i<p; i++){
-        parcial += vector1[i]*vector2[i];
-    }
-
    
-    if(rank ==0){      
+    if(rank ==0){
+	double * vector1 = malloc(n*sizeof(double));
+	double * vector2 = malloc(n*sizeof(double));
+
+	for(int j = 0; j < n ; j++){
+		vector1[j] = 1.0;
+		vector2[j] = 1.0;
+
+	}
+
+
+	for (int destination = 1; destination < size; destination++) {
+
+
+        	MPI_Send(vector1, p, MPI_DOUBLE, destination, tag, MPI_COMM_WORLD);
+        	MPI_Send(vector2 , p, MPI_DOUBLE, destination, tag+1, MPI_COMM_WORLD);
+        }
+
+
+	for(int i = 0; i<p; i++){
+		parcial += vector1[i]*vector2[i];
+	}
+
+
         total = parcial;
+
         for (source = 1; source < size; source++) {
             MPI_Recv(&parcial, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
             total += parcial;
         }
         printf("Total: %lf\n",total);
-        endtime   = MPI_Wtime();
+ 	endtime   = MPI_Wtime();
         printf("That took %f seconds\n",endtime-starttime);
     }
 
+
+
     else{
-        
-        MPI_Send(&parcial, 1, MPI_DOUBLE, destination, tag, MPI_COMM_WORLD);
+	double * vector1 = malloc(p*sizeof(double));
+	double * vector2 = malloc(p*sizeof(double));
+
+        MPI_Recv(vector1, p, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+        MPI_Recv(vector2, p, MPI_DOUBLE, 0, tag+1, MPI_COMM_WORLD, &status);
+
+	for(int i = 0; i<p; i++){
+       		parcial += vector1[i]*vector2[i];
+    	}
+
+        MPI_Send(&parcial, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD);
 
 
     }
@@ -62,3 +85,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
